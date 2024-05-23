3a5f827eb859b71e230b6ab4382c9cee188086d6
msgtv <linar_97@list.ru>
2024-05-23 20:38:56 +0500

    🔥

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,4 +1,12 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
+ENV = os.environ.get(
+    "ENV",
+    "django-insecure-4gwa)4@h&la@a3oyzw)^7f%1b(t!m*_f1f*cp@b8%o1myzmx_)"
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,13 +16,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4gwa)4@h&la@a3oyzw)^7f%1b(t!m*_f1f*cp@b8%o1myzmx_)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if ENV == "PROD" else True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', "*").split(',')
 
 # Application definition
 
@@ -77,11 +84,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'coredb',
-        'USER': 'core',
-        'PASSWORD': 'core',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get("DATABASE_NAME", "coredb"),
+        'USER': os.environ.get("DATABASE_USER", "core"),
+        'PASSWORD': os.environ.get("DATABASE_PASSWORD", "core"),
+        'HOST': os.environ.get("DATABASE_HOST", "localhost"),
+        'PORT': os.environ.get("DATABASE_PORT", "5432"),
     }
 }
 
@@ -121,7 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'static'
 
 # Media files
 MEDIA_ROOT = BASE_DIR / 'media'
